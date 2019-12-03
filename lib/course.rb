1e6c3e0d89a487461a86466bc9a47d3988988de2
Logan Smith <logsmith06@gmail.com>
2019-12-03 23:30:50 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -3,13 +3,13 @@ class Course
   attr_accessor :title, :schedule, :description
   @@all = []
 
-  def initialize 
-    @@all << self 
-  end 
+  def initialize
+    @@all << self
+  end
 
-  def self.all 
-    @@all 
-  end 
+  def self.all
+    @@all
+  end
 
 
 end

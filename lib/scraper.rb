de027a21f4f1f37b6140408217b1584ca018901c
Logan Smith <logsmith06@gmail.com>
2019-12-04 00:06:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -37,4 +37,15 @@ class Scraper
     get_courses
   end
 
+  def print_courses
+    self.make_courses
+    Course.all.each do |course|
+      if course.title && course.title != ""
+        puts "Title: #{course.title}"
+        puts "  Schedule: #{course.schedule}"
+        puts "  Description: #{course.description}"
+      end
+    end
+  end
+
 end
